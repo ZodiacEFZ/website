a3c0c6abc3905be097b1ba1ffe8916b76c5c12d5
SkyZH <iSkyZH@gmail.com>
2016-07-22 20:35:36 +0800

    apply new design

---
+++ _assets/js/index.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    menu: '#navbar-index',
+    lockAnchors: false,
+    anchors:['pgIntro', 'pgFRC', 'pgHsefz', 'pgSponsor'],
+    navigation: true,
+    navigationPosition: 'right',
+    showActiveTooltip: false,
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom',
+
+    css3: true,
+    scrollingSpeed: 700,
+    autoScrolling: true,
+    fitToSection: true,
+    fitToSectionDelay: 1000,
+    scrollBar: false,
+    easing: 'easeInOutCubic',
+    easingcss3: 'ease',
+    loopBottom: false,
+    loopTop: false,
+    loopHorizontal: false,
+    continuousVertical: false,
+    scrollOverflow: false,
+    scrollOverflowOptions: null,
+    touchSensitivity: 15,
+    normalScrollElementTouchThreshold: 5,
+    bigSectionsDestination: null,
+
+    controlArrows: false,
+    verticalCentered: true,
+    sectionsColor : ['#ccc', '#fff'],
+    responsiveWidth: 544,
+    responsiveHeight: 0,
+  });
+});
--- about.md
+++
@@ -1,6 +0,0 @@
----
-layout: page
-title: 关于
----
-
-这是一个由 FRC@EFZ 建设的站点。
