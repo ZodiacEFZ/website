4d8d253fa1963cd83c32c455746617abc128a8eb
SkyZH <iskyzh@gmail.com>
2018-08-04 15:30:07 +0800

    upgrade qiniu fix

--- _utils/ci_deploy_qiniu.sh
+++ _utils/ci_deploy_qiniu.sh
@@ -3,4 +3,5 @@ set -e # Exit with nonzero exit code if anything fails
 cd "${0%/*}"
 
 ./ci_qiniu.sh
+./qshell account $QINIU_ACCESSKEY $QINIU_SECRETKEY
 ./qshell qupload 4 sync.json
--- _utils/ci_generate.sh
+++ _utils/ci_generate.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 set -e
 cd "${0%/*}"
-
-envsubst < "sync.template.json" > "sync.json"
