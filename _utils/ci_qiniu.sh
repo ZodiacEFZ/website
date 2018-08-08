a87f10ccd22a236d4dce2adf4f4b89d9b9e8ec09
迟遲的星星球 <iSkyZH@gmail.com>
2018-08-08 16:07:31 +0800

    Use other source for qiniu

--- _utils/ci_qiniu.sh
+++ _utils/ci_qiniu.sh
@@ -2,6 +2,6 @@
 set -e
 cd "${0%/*}"
 
-wget http://devtools.qiniu.com/qshell-v2.2.0.zip
+wget https://github.com/ZodiacEFZ/qshell-dependency/raw/master/qshell-v2.2.0.zip
 unzip qshell-v2.2.0.zip
 cp qshell-linux-x64 qshell
